12c5e8c814afed98616a6c0161892d8bf43bae1f
Emery Tate <tateejt@gmail.com>
2020-01-12 21:10:38 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20200112211010) do
+
+  create_table "passenger_taxis", force: :cascade do |t|
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,8 +25,10 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
